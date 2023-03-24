1af3aa9acb49aa1f16e082fa153db7dfcc0e6c92
KodaiCoder <nutdeknon@gmail.com>
2023-03-24 18:28:46 +0700

    - adjust content & hero image width  - change base text size to 18px

--- src/lib/components/Contents/Content.svelte
+++ src/lib/components/Contents/Content.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="grid grid-flow-row gap-9 p-3 text-center items-center order-last lg:order-first lg:text-start lg:items-end lg:p-0 lg:w-3/4"
+	class="grid grid-flow-row gap-9 p-3 text-center items-center order-last lg:order-first lg:text-start lg:items-end lg:p-0 lg:w-4/5"
 >
 	<!--! HEADER -->
 	<div class="font-bold text-4xl text-almostBlack lg:text-7xl">
@@ -11,7 +11,7 @@
 	</div>
 
 	<!--! PARAGRAPH -->
-	<div class="flex flex-col justify-around gap-6 items-center h-full lg:items-start lg:w-4/5">
+	<div class="flex flex-col justify-around gap-6 items-center h-full lg:items-start">
 		<p class="font-medium">
 			Get your team in sync, no matter your location. Streamline processes, create team rituals, and
 			watch productivity soar.
--- src/lib/components/MainContainer.svelte
+++ src/lib/components/MainContainer.svelte
@@ -1,5 +1,5 @@
 <div
-	class="grid my-14 grid-flow-row gap-16 justify-items-center lg:gap-20 lg:grid-flow-col lg:grid-cols-2 lg:mx-14"
+	class="grid my-14 grid-flow-row gap-16 justify-items-center  lg:grid-flow-col lg:grid-cols-2 lg:mx-14"
 >
 	<slot />
 </div>
